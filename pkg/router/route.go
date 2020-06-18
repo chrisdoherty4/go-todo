f7c8e0dda6f9770d5cfd7eb41bc16076d88bde87
Chris Doherty <chris.doherty@alteryx.com>
2020-06-17 21:49:25 -0600

    Rework router to adhere to Routable interface

--- pkg/router/route.go
+++ pkg/router/route.go
@@ -1,10 +1,13 @@
 package router
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 // Route defines
 type Route struct {
-	Routable
 	method  string
 	path    string
 	handler Handler
@@ -25,22 +28,51 @@ func (r *Route) Handler() Handler {
 	return r.handler
 }
 
+// NewRoute creates a new Route instance using the group attributes.
+func (r *Route) NewRoute(method, path string) *router.Route {
+	if g.pathPrefix != "" {
+		path = fmt.Sprintf("%v/%v", g.pathPrefix, strings.Trim(path, "/"))
+	}
+
+	return NewRoute(method, path)
+}
+
+// Get creates a new router.MethodGet Route using the group attributes.
+func (r Route) Get(path string) *Route {
+	return g.NewRoute(MethodGet, path)
+}
+
+// Post creates a new router.MethodPost Route using the group attributes
+func (r Route) Post(path string) *Route {
+	return g.NewRoute(MethodPost, path)
+}
+
+// Put creates a new router.MethodPut Route using the group attributes
+func (r Route) Put(path string) *Route {
+	return g.NewRoute(MethodPut, path)
+}
+
+// Delete creates a new router.MethodDelete Route using the group attributes
+func (r Route) Delete(path string) *Route {
+	return r.NewRoute(MethodDelete, path)
+}
+
 // Match is used by the Router to match requests to Routes.
-func (r *Route) Match(req *http.Request) bool {
+func (r Route) Match(request *http.Request) bool {
 	// TODO: Create more sophisticated algorithm for handling path tokens
-	if r.method != req.Method {
+	if r.method != request.Method {
 		return false
 	}
 
-	if r.URL.Path != r.path {
+	if r.path != request.URL.Path {
 		return false
 	}
 
 	return true
 }
 
-func (r *Route) Route(w http.ResponseWriter, r *http.Request) {
-	r.handler.Handle(w, r)
+func (r *Route) Route(writer http.ResponseWriter, request *http.Request) {
+	r.handler.Handle(writer, request)
 }
 
 // NewRoute creates a new Route instance.
--- pkg/router/routefactory.go
+++ pkg/router/routefactory.go
@@ -1,4 +1,12 @@
 package router
 
+// RouteFactory creates routes.
 type RouteFactory interface {
+	Get(string) *router.Route
+
+	Post(string) *router.Route
+
+	Put(string) *router.Route
+
+	Delete(string) *router.Route
 }
